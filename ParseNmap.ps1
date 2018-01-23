98a37ae379777efac9a1a0aa56cc039a7816a61c
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-01-23 15:51:44 +0000

    Create ParseNmap.ps1

---
+++ ParseNmap.ps1
@@ -0,0 +1,49 @@
+# Check a file has been passed
+param([string]$path="")
+
+if ($path -eq "") {
+    Write-Host "Missing Path"
+    Exit
+}
+
+
+[xml]$holder = Get-Content $path
+
+$block = $holder.nmaprun
+# Powershell arrays are annoying
+# Enumerate total number of ports open
+$a = $holder.nmaprun.host.ports.port | Measure-Object -Line | Select -ExpandProperty Lines
+$role = New-Object string[] $a
+$c = 0
+foreach ($obj in $block) {
+    $ips = $holder.nmaprun.host.ipaddr | Select -ExpandProperty Addr
+    $hostname = $block.host.hostnames.hostname | Select -ExpandProperty Name
+    $portz = $block.host.ports
+    foreach ($t in $portz) {
+        $box = $t.port
+        $cat = $t.port | Select -ExpandProperty portid
+        $stat = $t.port | Select -ExpandProperty state
+        foreach ($b in $box) {
+            if ($b -eq "445") {
+                $role[$c] = "Probable Windows - Port 445 in use"
+            }
+            else {
+                if ($b -eq "22") {
+                    $role[$c] = "Nix or Networking - Port 22 in use"
+                    }
+                else {
+                    $role[$c] = "Unknown"
+                    }
+                }
+        }
+        $c = $c + 1
+    }
+}
+
+
+$c = 0
+foreach ($a in $ips) { 
+    # Error Check
+    Write-Host $ips[$c] " - " $role[$c]
+    $c = $c + 1 
+}
