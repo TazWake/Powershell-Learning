333184fe8e22a45a45a2f6176fa636549e985c7f
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-04-25 11:50:13 +0100

    Create iprecon.ps1

---
+++ DFIR/iprecon.ps1
@@ -0,0 +1,16 @@
+<#
+.SYNOPSIS
+A script to assist incident handlers in looking up suspicious IP address, either found in logs or provided through other IR Tools
+.DESCRIPTION
+Carries out a check on multiple websites to gather information.
+.Example
+iprecon.ps1 -TargetIP 8.8.8.8
+.Example 
+
+.LINK
+https://github.com/TazWake/Powershell-Learning/blob/master/huntBotNets.ps1
+.PLANNED IMPROVEMENTS
+1) Output to a file
+2) Allow multiple input IPs
+3) Improve quality of search tools utilised.
+#>
