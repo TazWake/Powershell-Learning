1b904fe76d7b2c4d52094eeaeabe344a96b94335
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-05-01 18:04:29 +0100

    Create ipblacklistcheck.ps1

---
+++ ipblacklistcheck.ps1
@@ -0,0 +1,19 @@
+<#
+check an IP address against blocklists
+https://raw.githubusercontent.com/ktsaou/blocklist-ipsets/master/firehol_level1.netset
+
+#>
+param(
+    [Parameter(Mandatory=$true)][string]$suspIP
+    )
+
+$webcheck = Invoke-WebRequest "https://raw.githubusercontent.com/ktsaou/blocklist-ipsets/master/firehol_level1.netset"
+$webcheck.content | Out-File .\blacklist.txt
+
+$checks = Get-Content .\blacklist.txt
+
+if ($checks -contains $suspIP) {
+    Write-Host "This IP is likely to be malicious"
+    } else {
+    Write-Host "No hits found"
+    }
