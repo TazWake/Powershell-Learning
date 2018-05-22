f76e2aad24c1f6234ad6f2b4191aa2d0b4832e2c
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-05-22 13:30:17 +0100

    Update timestamps.ps1

--- DFIR/timestamps.ps1
+++ DFIR/timestamps.ps1
@@ -10,9 +10,11 @@ If no path is provided with the execution it looks at files in the current path.
 
 .EXAMPLE
 
-
+timestamps.ps1
 
 .EXAMPLE
+
+timestamps.ps1 -targetPath C:\Windows\temp -outputPath D:\incidentresponse\
 
 .LINK
 
@@ -20,19 +22,43 @@ https://github.com/TazWake/Powershell-Learning/blob/master/DFIR/timestamps.ps1
 
 .NOTES
 To Do List
-1) Output to file
+
+1) Clean up output to make CSV useable
+
 #>
 
 param(
-    [Parameter(Mandatory=$false)][string]$targetPath
+    [Parameter(Mandatory=$false)][string]$targetPath,
+    [Parameter(Mandatory=$false)][string]$outputPath
     )
 
-$path = "*"
+function outData {
+    $output = "$outputPath\timestamps.csv"
+    if ((Test-Path -Path $output) -eq $false) {
+        New-Item $output -ItemType file
+        'Name,Size(bytes),Creation Time (UTC),Last Access Time (UTC), Last Write Time (UTC)' | Out-File -FilePath $output -Append -Encoding ascii -Force
+    }
+    return $output
+}
+function writeCSV ($scanpath, $outpath) {
+    $box = Get-ItemProperty -Path $scanpath
+    $data = $box.Name + "," + $box.Length + "," + $box.CreationTimeUtc + "," + $box.LastAccessTimeUtc + "," + $box.LastWriteTimeUtc
+    $data | Out-File -FilePath $outpath -Append -Encoding ascii -Force
+}
+
+$scanpath = "*"
 
 if ($targetPath) {
-    $path = $targetPath
+    $scanpath = $targetPath
     }
 
 Write-Host "Starting collection"
-Get-ItemProperty -Path $path | Format-list -Property Name, Length, CreationTimeUtc, LastAccessTimeUtc, LastWriteTimeUtc
+if ($outputPath) {
+    $filepath = outData
+    $filepath = "$outputPath\timestamps.csv" <# fudge to get round an odd issue #>
+    writeCSV $scanpath $filepath
+}
+
+Get-ItemProperty -Path $scanpath | Format-list -Property Name, Length, CreationTimeUtc, LastAccessTimeUtc, LastWriteTimeUtc
+
 Write-Host "Collection completed"
